5d47ade1f5b736b696121da6f4829c7dc727e78b
Filippo De Luca <filippo.deluca@ovoenergy.com>
2020-10-13 16:14:47 +0200

    Allow specify percentiles in timer creation

--- core/src/main/scala/com/ovoenergy/meters4s/Reporter.scala
+++ core/src/main/scala/com/ovoenergy/meters4s/Reporter.scala
@@ -50,7 +50,7 @@ trait Reporter[F[_]] {
     * @param tags tags associated with this timer
     * @return an effect that evaluates to a timer instance
     */
-  def timer(name: String, tags: Map[String, String] = Map.empty): F[Timer[F]]
+  def timer(name: String, tags: Map[String, String] = Map.empty, percentiles: Set[Double] = Set.empty): F[Timer[F]]
 
   /**
     * Create a gauge
@@ -230,11 +230,12 @@ private class MeterRegistryReporter[F[_]](
         }
       }
 
-  def timer(name: String, tags: Map[String, String]): F[Timer[F]] =
+  def timer(name: String, tags: Map[String, String], percentiles: Set[Double]): F[Timer[F]] =
     F.delay {
         micrometer.Timer
           .builder(metricName(name))
           .tags(effectiveTags(tags))
+          .publishPercentiles(percentiles.toSeq:_*)
           .register(mx)
       }
       .map { t =>
